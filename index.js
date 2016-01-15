2020ba06fe8e7d2d00f8a3d6a02d4340d58f106a
Kashav Madan <kshvmdn@gmail.com>
2016-01-14 20:51:52 -0500

    Added IMDb request with npm pkg `movie`

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 #! /usr/bin/env node
-"use strict";
+'use strict';
 
 const Promise = require('bluebird');
 const colors = require('colors');
-const inquirer = require('inquirer');
-const jsonfile = require('jsonfile');
-const movie = require('node-movie');
+const movie = require('node-movie').getByID;
 const tpb = require('thepiratebay');
 const torrentStream = require('torrent-stream');
 
 console.log('So you wanna see a movie?'.blue);
+var getJSON = function(token) {
+	return new Promise(function(resolve, reject) {
+		movie(token, function(err, data) {
+			resolve(data);
+		});
+	});
+}
 
+let token = process.argv[2];
+getJSON(token).then(function(data) {
+}).then(function(data) {
+}).catch(function(e) {
+	console.log(e);
+});
