123ffff89e1971c5ca350ea785779a557f8bff92
Kashav Madan <kshvmdn@gmail.com>
2016-02-15 15:54:33 -0500

    Add top 250 scraper

--- scrapers/top250.py
+++ scrapers/top250.py
@@ -2,20 +2,33 @@
 Scrape the top 250 movie tokens from IMDb
 """
 
+import json
 import requests
 from bs4 import BeautifulSoup
 
 
 def scrape(url):
-    return parse(request(url))
+    return prepare(parse(request(url)))
 
 
 def request(url):
-    return requests.get('http://www.imdb.com/chart/top')
+    return requests.get(url)
 
 
-def parse(response):
-    return response.content
+def parse(resp):
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    tokens = []
+    for tr in soup.find('table', class_='chart').find_all('tr'):
+        if tr.find('td', class_='ratingColumn'):
+            token = tr.find('td', class_='watchlistColumn').find('div')['data-tconst']
+            tokens.append(token)
+    return tokens
+
+
+def prepare(tokens):
+    f = open( './show-me-a-movie/src/tokens.json', 'w' )
+    f.write(json.dumps(tokens, indent=2))
+    f.close()
 
 
 if __name__ == '__main__':
