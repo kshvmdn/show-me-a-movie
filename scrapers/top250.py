3e90928474175528716127202ee88cd1572bc2fd
Kashav Madan <kshvmdn@gmail.com>
2016-02-15 15:38:12 -0500

    Scraper bootstrapping

--- scrapers/top250.py
+++ scrapers/top250.py
@@ -1,6 +1,23 @@
 """
-Scrape IMDb's top 250 movie tokens
+Scrape the top 250 movie tokens from IMDb
 """
 
 import requests
 from bs4 import BeautifulSoup
+
+
+def scrape(url):
+    return parse(request(url))
+
+
+def request(url):
+    return requests.get('http://www.imdb.com/chart/top')
+
+
+def parse(response):
+    return response.content
+
+
+if __name__ == '__main__':
+    imdb_url = 'http://www.imdb.com/chart/top'
+    scrape(imdb_url)
