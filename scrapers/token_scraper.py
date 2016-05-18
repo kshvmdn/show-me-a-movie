cdf43773089c726f448a173ebb27308ee76afcda
Kashav Madan <kshvmdn@gmail.com>
2016-05-17 21:45:04 -0400

    Check that element exists before scraping

--- scrapers/token_scraper.py
+++ scrapers/token_scraper.py
@@ -5,21 +5,22 @@ from bs4 import BeautifulSoup
 
 class Scraper():
     """Scrape all movie tokens off of a IMDb list."""
-    
+
     def __init__(self, url):
         self.url = url
-    
+
     def request(self):
         return requests.get(self.url).text
-    
+
     def run(self):
         resp = requests.get(self.url)
         soup = BeautifulSoup(self.request(), 'html.parser')
         tokens = []
-        for tr in soup.find('table', class_='chart').find_all('tr'):
-            token = tr.find('td', class_='watchlistColumn').find('div')['data-tconst']
-            if token:
-                tokens.append(token)
+        for tr in soup.find('table', class_='chart full-width').find_all('tr'):
+            td = tr.find('td', class_='watchlistColumn')
+            if td and td.find('div') and td.find('div')['data-tconst']:
+                tokens.append(td.find('div')['data-tconst'])
+        print('%d tokens scraped.' % len(tokens))
         save_json(tokens)
 
 
